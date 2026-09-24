6d64811940974311f828c005b472414765884451
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-09-25 01:22:00 +0200

    Set up JFrame for Pac Man game
    Initialize JFrame for Pac Man game with specified dimensions.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
  public class App {
     public static void main(String[] args)throws Exception{
-        System.out.println("Hello world! ");
+     int rows = 21;
+     int columns = 19;
+     int tileSize = 32; // pixel 
+     int boardHeight = rows * tileSize;  // 672 hög
+     int boardWidth = columns * tileSize;// 608 bred
+
+     JFrame frame = new JFrame("Pac Man");
+     frame.setVisible(true);
+     frame.setSize(boardWidth,boardHeight);
+     frame.setLocationRelativeTo(null);
+     frame.setResizable(false);
+
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+     
     }
 }
